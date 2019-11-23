350451d9beccea617bc546f75d0a2649f5511905
Njabulo96 <njabulomlang96@gmail.com>
2019-11-23 10:28:09 +0200

    sfsfas

---
+++ functions/src/index.ts
@@ -0,0 +1,11 @@
+const functions = require('firebase-functions');
+
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+exports.createAccount = functions.firestore.document('sales/{docid}').onCreate((change: any, context:any) => {
+  console.log('Document change', change.data(), 'Document context', context);
+  // const email = change.get('email');
+  // const password = change.get('pwd');
+  
+})
