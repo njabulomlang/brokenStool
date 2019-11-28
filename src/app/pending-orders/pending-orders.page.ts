eb1ac9d6d6bfcc0bf16fa0140483c1951343e561
Njabulo96 <njabulomlang96@gmail.com>
2019-11-28 10:14:34 +0200

    pending orders

--- src/app/pending-orders/pending-orders.page.ts
+++ src/app/pending-orders/pending-orders.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import * as firebase from 'firebase';
+import { NavController } from '@ionic/angular';
+import { NavigationExtras } from '@angular/router';
+
 @Component({
   selector: 'app-pending-orders',
   templateUrl: './pending-orders.page.html',
@@ -9,17 +12,28 @@ export class PendingOrdersPage implements OnInit {
   dbOrder = firebase.firestore().collection('Order');
   uid = firebase.auth().currentUser.uid;
   myOrder = [];
-  constructor() { }
+  constructor(public navCtrl: NavController) { }
 
   ngOnInit() {
-    this.dbOrder.where('userID','==',this.uid).onSnapshot((res)=>{
+    this.dbOrder.where('userID', '==', this.uid).onSnapshot((res) => {
       this.myOrder = [];
-      res.forEach((doc)=>{
-        this.myOrder.push({info:doc.data(), id: doc.id});
+      res.forEach((doc) => {
+        this.myOrder.push({ info: doc.data(), id: doc.id });
       })
     })
   }
-  orderTrack(id,data) {
+  orderTrack(id, data) {
+    let navigationExtras: NavigationExtras = {
+      queryParams: {
+        id: id,
+        data: data,
+        //currency: JSON.stringify(currency),
+        // refresh: refresh
+      }
+    }
+    this.navCtrl.navigateForward(['track-orders',id], navigationExtras) 
+   // console.log('My order id ',id, 'data ', data );
     
   }
+
 }
