64f361ea747d6da9616f6e20e4fcd6fbe097e6d8
Njabulo96 <njabulomlang96@gmail.com>
2019-12-03 09:33:16 +0200

    sccsc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,8 +22,7 @@ export class AppComponent {
     this.initializeApp();
     firebase.initializeApp(firebaseConfig);
     this.checkUser();
-   // console.log("UID ", firebase.auth().currentUser.uid);
-    
+   // console.log("UID ", firebase.auth().currentUser.uid)
   }
   checkUser() {
     //firebase.auth().settings.appVerificationDisabledForTesting = true;
