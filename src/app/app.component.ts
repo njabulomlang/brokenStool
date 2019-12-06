2c323c35498de3c18b649a60fad2e122559887d9
Zanele Refiloe nkosi <50693705+ZaneleNkosi@users.noreply.github.com>
2019-12-06 11:34:25 +0200

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -30,7 +30,7 @@ export class AppComponent {
     //firebase.auth().settings.appVerificationDisabledForTesting = true;
     firebase.auth().onAuthStateChanged((res) => {
       if (res) {
-        this.router.navigateByUrl('login')
+        this.router.navigateByUrl('home')
        // console.log("User..", res.phoneNumber);
       } else {
         this.router.navigateByUrl('login')
