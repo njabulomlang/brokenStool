f5b7e7dd3aef4bb02995734f57c11e094befda38
Zanele Refiloe nkosi <50693705+ZaneleNkosi@users.noreply.github.com>
2020-01-10 16:37:53 +0200

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -84,7 +84,11 @@ const routes: Routes = [
   {
     path: 'search',
     loadChildren: () => import('./search/search.module').then( m => m.SearchPageModule)
+  },  {
+    path: 'info',
+    loadChildren: () => import('./info/info.module').then( m => m.InfoPageModule)
   },
+
 
 
 ];
